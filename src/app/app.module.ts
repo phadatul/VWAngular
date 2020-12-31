86dbd1b13e7527a58a5adde09e9ee3f62c2e1060
phadatul <phadatul@gmail.com>
2020-12-31 15:02:49 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { HomeComponent } from "./home/home.component";
