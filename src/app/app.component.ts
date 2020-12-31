54a8ef61697fbc81a8cf874ba15879a84326fc69
phadatul <phadatul@gmail.com>
2020-12-31 16:14:44 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,24 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Angular " + VERSION.major;
+  name = "Angular ";
   title = "Welcome to Angular";
+  arr = ["Apple", "mango", "Banana", "Orange", "Pineapple", "Pears"];
+  loginFlag = false;
+  count = 1;
+
+  hello() {
+    this.title = "You clicked button";
+    console.log("hello world");
+    if (this.loginFlag == false) {
+      this.loginFlag = true;
+    } else {
+      this.loginFlag = false;
+    }
+  }
+  bye() {
+    console.log("hi this is from text box");
+    this.name = "hi" + this.count;
+    this.count++;
+  }
 }
